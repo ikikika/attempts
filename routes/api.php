6ebf59c26d95b4c55ee3db1db11db79b14e5bd90
ikikika <iki.jito@gmail.com>
2018-11-18 13:48:11 +0800

    set up mmc for user auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix'=>'api'], function(){
+  Route::post('login', 'API\UserController@login');
+  Route::post('register', 'API\UserController@register');
+});
+Route::group(['prefix'=>'admin', 'middleware' => 'auth:api'], function(){
+  Route::post('user', 'API\UserController@user');
+  Route::get('logout', 'API\UserController@logout');
 });
