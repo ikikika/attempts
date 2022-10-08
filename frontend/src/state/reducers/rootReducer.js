2e1459877542faae7d1e822472f96e42a33d82ca
iki <iki.jito@gmail.com>
2022-10-08 17:52:57 +0800

    movies-api

--- frontend/src/state/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import movieReducer from "./MovieReducer";
-
-const rootReducer = combineReducers({ moviesState: movieReducer });
-
-export default rootReducer;
