40b53819169db3bed359794fb40aec2cfcb4a7a3
iki <iki.jito@gmail.com>
2022-12-11 04:30:38 +0800

    getters and setters

--- nextGenJS/05-getters&Setters.ts
+++ nextGenJS/05-getters&Setters.ts
@@ -2,8 +2,6 @@ class Department {
   // private readonly id: string;
   // private name: string;
   protected employees: string[] = [];
-  // private cannot be accessed outside this class
-  // if we want to inherit this property, we need to use protected
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -35,8 +33,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
 
   addEmployee(name: string) {
@@ -48,6 +63,7 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -70,7 +86,9 @@ console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
 
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
